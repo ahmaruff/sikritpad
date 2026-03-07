a675ece40f0000bf840c1ae97d8b16d6b70fcb25
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2026-03-07 23:21:40 +0700

    chore: remove unused file

--- app.js
+++
@@ -1,8 +0,0 @@
-function unlock(password) {
-    console.log(password);
-}
-
-document.getElementById("open").onclick = () => {
-    const password = document.getElementById("password").value;
-    unlock(password);
-};
